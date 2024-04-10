7829ffd3f2df1ef86e0132d279fff4159a8925ea
Justin Van Patten <jvp@justinvp.com>
2024-04-10 04:28:10 -0700

    Use the latest version of Node.js (#72)
    This fixes failures due to packages not supporting Node 14.x.
    
    Additionally, fix the CI script for the `node-local-program-tsnode` to install dependencies for the program. This has apparently [never worked in CI](https://github.com/pulumi/automation-api-examples/actions/runs/7991360249/job/21822347128#step:10:40), but it wasn't previously reported as a failure. Now with the Node.js upgrade, it's failing in CI.
    
    Finally, fix an actual problem with the `node-local-program-tsnode` program. It was failing with an "Access Denied" error trying to create the bucket policy. Fix this up to use the latest way of doing it.

--- nodejs/localProgram-tsnode/website/index.ts
+++ nodejs/localProgram-tsnode/website/index.ts
@@ -2,11 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { PolicyDocument } from "@pulumi/aws/iam";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
 
 // Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("s3-website-bucket", {
@@ -14,6 +9,21 @@ const siteBucket = new aws.s3.Bucket("s3-website-bucket", {
         indexDocument: "index.html",
     },
 });
+
+// Configure ownership controls for the new S3 bucket
+const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+    bucket: siteBucket.bucket,
+    rule: {
+        objectOwnership: "ObjectWriter",
+    },
+});
+
+// Configure public ACL block on the new S3 bucket
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: siteBucket.bucket,
+    blockPublicAcls: false,
+});
+
 const indexContent = `<html><head>
 <title>Hello S3</title><meta charset="UTF-8">
 </head>
@@ -23,34 +33,13 @@ const indexContent = `<html><head>
 
 // write our index.html into the site bucket
 let object = new aws.s3.BucketObject("index", {
+    acl: "public-read",
     bucket: siteBucket,
     content: indexContent,
     contentType: "text/html; charset=utf-8",
     key: "index.html"
+}, {
+    dependsOn: [ownershipControls, publicAccessBlock]
 });
-
-// Create an S3 Bucket Policy to allow public read of all objects in bucket
-function publicReadPolicyForBucket(bucketName: string): PolicyDocument {
-    return {
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Principal: "*",
-            Action: [
-                "s3:GetObject"
-            ],
-            Resource: [
-                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
-            ]
-        }]
-    };
-}
-
-// Set the access policy for the bucket so all objects are readable
-let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.bucket, // refer to the bucket created earlier
-    policy: siteBucket.bucket.apply(publicReadPolicyForBucket) // use output property `siteBucket.bucket`
-});
-
 
 export const websiteUrl = siteBucket.websiteEndpoint;
